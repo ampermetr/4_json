97c2082c9838a7cd3b7d5bff01aa1a560b969222
ampermetr <mlechnik1988@mail.ru>
2016-10-02 23:52:06 +0000

    Update pprint_json.py

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,19 @@
 import json
+import pprint
 
 
 def load_data(filepath):
-    pass
+    with open (filepath, 'r', encoding = 'utf8') as file:
+        data = json.load(file)
+    return data
 
 
 def pretty_print_json(data):
-    pass
+    data.insert(0, data)
+    pprint.pprint(data)
 
 
 if __name__ == '__main__':
-    pass
+    filepath = input('Введите путь к файлу' + '\n')
+    data = load_data(filepath)
+    
